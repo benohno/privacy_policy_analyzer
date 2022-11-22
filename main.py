a6304576bb12bbc82d35281a8121728383bccf76
Ben Ohno <benjaminohno@gmail.com>
2022-11-22 17:48:23 -0500

    adding time to read

--- main.py
+++ main.py
@@ -53,15 +53,17 @@ st.write('The url input is: ', privacy_policy_url)
 if privacy_policy_url == '':
     reading_score = 'NA'
     word_total = 'NA'
+    time_to_read = 'NA'
 
 else:
     reading_score = readability_score(privacy_policy_str)
     word_total = word_count(privacy_policy_str)
+    time_to_read = round(word_total/250)
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Reading Grade Level", reading_score)
-col2.metric("Total Length", word_total)
-col3.metric("Place Holder metric", "22", "4%")
+col2.metric("Total Length", str(word_total) + ' words')
+col3.metric("Estimated Time to Read", str(time_to_read) + ' minutes')
 
 
 st.text("🚩 - Risk #1")
