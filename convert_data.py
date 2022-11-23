c97f35a89d643feb757ada1b53659086ef712763
michaelmalave-ucb <michaelmalave@berkeley.edu>
2022-11-23 11:14:45 -0800

    adding multilabel cnn initial creation

--- convert_data.py
+++ convert_data.py
@@ -30,9 +30,11 @@ combined_csv = combined_csv.sort_values(by=['outcome', 'source', 'text']).reset_
 # get the distinct classes
 # for each class, create a file with that line of text and name it as the original file name, f
 # then export to file
+
 classes = combined_csv['outcome'].unique()
 os.makedirs('../train', exist_ok=True)
 # combined_csv.to_csv(f'../train/train_data.csv', index=False)
+"""
 for c in classes:
     c = re.sub(r'\s+', '', c)
     os.makedirs('../train/{}'.format(c), exist_ok=True)
@@ -42,3 +44,27 @@ for c in classes:
         filename = '../train/{}/{}-{}.txt'.format(c, source, index)
         with open(filename, 'w+') as f:
             f.write(data['text'])
+"""
+
+
+def encode_and_bind(original_dataframe, feature_to_encode):
+    """
+    this one hot encodes a rows with potentially multiple labels in a single column
+    :param original_dataframe:
+    :param feature_to_encode:
+    :return: multi one hot encoded dataframe
+    """
+    dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
+    res = pd.concat([original_dataframe, dummies], axis=1)
+    res.drop(columns=[feature_to_encode])
+    res = res.groupby(['text', 'source']).sum().reset_index()
+    return res
+
+
+pd.set_option('display.max_columns', None)
+combined_csv = encode_and_bind(combined_csv, 'outcome')
+os.chdir("../")
+os.makedirs('train_csv', exist_ok=True)
+combined_csv.to_csv('train_csv/policies_train.csv', index=False)
+df = pd.read_csv('train_csv/policies_train.csv')
+print(df.head(4))
